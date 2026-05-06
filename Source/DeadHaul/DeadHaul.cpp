835c2e66a68615ff39bb8ad93ca7544d652a0d97
Johnny Paschen <johnpaschen@gmail.com>
2026-05-06 13:56:25 -0700

    Project Setup and first commit

---
+++ Source/DeadHaul/DeadHaul.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadHaul.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DeadHaul, "DeadHaul" );
